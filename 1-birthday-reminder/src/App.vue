6a5dadb898ba4f2f6d7620f3e4c5fecf6c872e00
Debayo Tomisin <debayotomisin07@gmail.com>
2021-09-03 21:22:26 -0700

    state wahala overcomed

--- 1-birthday-reminder/src/App.vue
+++ 1-birthday-reminder/src/App.vue
@@ -1,13 +1,64 @@
 <template>
-  <div>
-    <h1> Gerogeous </h1>
-  </div>
+  <main>
+    <Lists :people="people" />
+  </main>
 </template>
 
 <script>
+import Lists from './components/List'
 
 export default {
+  
   name: 'App',
+  components: {
+    Lists
+  },
+  data() {
+    return{
+      people: []
+    }
+  },
+
+  created() {
+    this.people = 
+    [
+      {
+        id: 1,
+        name: 'Bertie Yates',
+        age: 29,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      },
+      {
+        id: 2,
+        name: 'Hester Hogan',
+        age: 32,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      },
+      {
+        id: 3,
+        name: 'Larry Little',
+        age: 36,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      },
+      {
+        id: 4,
+        name: 'Sean Walsh',
+        age: 34,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      },
+      {
+        id: 5,
+        name: 'Lola Gardner',
+        age: 29,
+        image:
+          'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      },
+    ]
+  }
 }
 </script>
 
---
+++ 1-birthday-reminder/src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :key="person.id"  v-for="person in people">
+        <h1>{{person.name}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Lists',
+        props: {
+            people: Array
+        }
+    }
+    
+</script>
+
+<style scoped>
+
+</style>
