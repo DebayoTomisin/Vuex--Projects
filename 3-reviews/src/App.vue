3be7a8e74193c7efbe31763df0345fdf8df7b56b
Debayo Tomisin <debayotomisin07@gmail.com>
2021-09-18 23:31:01 -0700

    review component shenanigans

--- 3-reviews/src/App.vue
+++ 3-reviews/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div>
-    Patience and GoodLuck
-  </div>
+  <main>
+    <section class="contiainer">
+      <h2>Our Reviews </h2>
+      <div class="underline"></div>
+      <Reviews  />
+    </section>
+  </main>
 </template>
 
 <script>
+import Reviews from './components/Reviews'
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    Reviews
+  },
+  
+  
 }
 </script>
 
---
+++ 3-reviews/src/components/Reviews.vue
@@ -0,0 +1,46 @@
+<template>
+    <article class="review">
+
+    </article>
+</template>
+
+<script>
+    import reviewData from './data'
+    export default {
+        name: 'Reviews',
+        data () {
+            return{
+                data: [],
+                index: 0
+            }
+        },
+        created () {
+            this.data = reviewData
+        },
+        methods: {
+            checkNumbers (number) {
+                if (number < 0){
+                    return this.data.length
+                }
+                if (number > this.data.length - 1){
+                    return 0
+                }
+                return number
+            },
+
+            randomPerson () {
+                let randomNumber = Math.floor(Math.random() * this.data.length)
+                if (randomNumber === this.index) {
+                    randomNumber += 1
+                }
+                this.index = this.checkNumbers(randomNumber)
+            }
+
+        }
+    }
+
+</script>
+
+<style src='../assets/index.css' scoped>
+
+</style>
