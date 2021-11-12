512d7288fcc14ddd4b5a86eb2dce8c59a2625252
Debayo Tomisin <debayotomisin07@gmail.com>
2021-11-12 08:52:55 -0800

    bug with toggling

--- 4-accordion/src/components/Question.vue
+++ 4-accordion/src/components/Question.vue
@@ -1,13 +1,27 @@
 <template>
     <article class='question'>
-        Question Component
+       <div>
+            <h4>{{question.title}}</h4>
+            <button class='btn' @click="$emit('toggleInfo')" v-if="showInfo"> - </button>
+            <button class='btn' @click="$emit('toggleInfo')" v-else> + </button>
+       </div>
+       <p v-if='showInfo'>{{question.info}}</p>
     </article>
 </template>
 
 <script>
     export default {
         name: 'Question',
+        props: {
+            showInfo : Boolean,
+            question: {
+                info: String,
+                title: String,
+            },
+        },
+        emit : ['toggleInfo'],
     }
+    
 </script>
 
 <style src="../assets/index.css">
