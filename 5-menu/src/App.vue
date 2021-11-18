ae31dc91a1490de0ce1c33ec157b4f216e826e21
Debayo Tomisin <debayotomisin07@gmail.com>
2021-11-18 09:23:25 -0800

    project set up, layouts and components

--- 5-menu/src/App.vue
+++ 5-menu/src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="menu section">
+    <div class="title">
+      <h2>Our Menu</h2>
+    </div>
+    <div class="underline"></div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/data'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return{
+      menu : Menu
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./assets/index.css">
+
 </style>
