0b15a87841cbd72db5d5b532cae5f2cd95c90e7e
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-03 01:42:02 -0800

    methods and stuff

---
+++ 6-tabs/src/components/Tabs.vue
@@ -0,0 +1,45 @@
+<template>
+    <section v-if="loading" class="section-loading">
+        <h2>Loading... </h2>
+    </section>
+    <section v-if="!loading" class="dection">
+        <h4>Make Room</h4>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Tabs',
+        data() {
+            return {
+                jobs: {},
+                value: 0,
+                loading: true,
+                url : 'https://course-api.com/react-tabs-project',
+
+            }
+        },
+        methods: {
+            fetchJobs : async () => {
+                const response = await fetch(this.url)
+                const data = await response.json(response)
+
+                this.jobs = data
+                this.loading = false
+            }
+        },
+        watch: {
+            handler() {
+                this.fetchJobs()
+            }
+        },
+        mounted() {
+            this.fetchJobs()
+            console.log(this.jobs)
+        }
+    }
+</script>
+
+<style src='../assets/index.css' scoped>
+
+</style>
