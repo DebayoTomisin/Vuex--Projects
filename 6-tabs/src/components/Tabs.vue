a849941272511c293aebdb1e52a67a8b607f037d
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-11 15:26:05 -0800

    complete working project, incorrect styling

--- 6-tabs/src/components/Tabs.vue
+++ 6-tabs/src/components/Tabs.vue
@@ -9,7 +9,18 @@
         </div>
         
         <div class="jobs-center">
-            
+            <div v-for="(job, index) in jobs" :key="index" class="btn-container">
+                <button v-on:click="setValue(index)" class="job-btn">{{job.company}}</button>
+            </div>
+
+            <article class="job-info">
+                <h3>{{jobs[value].title}}</h3>
+                <h4>{{jobs[value].company}}</h4>
+                <p class="job-date">{{jobs[value].dates}}</p>
+                <div v-for="(duty, index) in jobs[value].duties" :key="index" class="job-desc">
+                    <p>{{duty}}</p>
+                </div>
+            </article>
         </div>
     </section>
 </template>
@@ -20,7 +31,7 @@
         data() {
             return {
                 jobs: [],
-                value: 0,
+                value: 2,
                 loading: true,
                 url : 'https://course-api.com/react-tabs-project',
 
@@ -31,17 +42,21 @@
                 const response = await fetch(this.url)
                 const data = await response.json(response)
                 this.jobs = data
+                this.loading = false
+            },
+            setValue(payload) {
+                this.value = payload
             }
         },
-        
+
+        computed: {
+        },
         created() {
             this.fetchJobs()
-            console.log(this.jobs)
-            this.loading = false
         }
     }
 </script>
 
-<style src='../assets/index.css' scoped>
+<style src='../assets/index.css'>
 
 </style>
