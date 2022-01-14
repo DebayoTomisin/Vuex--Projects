7c04d96362fb8b379bc47595193b41c03935088a
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-13 18:52:21 -0800

    app vue shenanigans

--- 7-slider/src/App.vue
+++ 7-slider/src/App.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="title"> <h2><span>/</span>Reviews</h2></div>
     <div class="section-center">
-      <People :people="peopleData" :index="index" />
+      <People :people="peopleData" :index="index" v-model="index"/>
       <Button />
     </div>
   </section>
@@ -26,31 +26,47 @@ export default {
   },
 
   methods: {
-    nextSlide () {
-      function setNextIndex() {
-        let index = this.index + 1
-        if(index < this.peopleData.length - 1) {
-          index = 0
-        }
-        return index
+    // nextSlide () {
+    //   function setNextIndex() {
+    //     let index = this.index + 1
+    //     if(index < this.peopleData.length - 1) {
+    //       index = 0
+    //     }
+    //     return index
+    //   }
+    //   this.index = setNextIndex()
+    // },
+
+    afterSlide () {
+      let index = this.index + 1
+      if (index < this.peopleData.length - 1){
+        index = 0
       }
-      this.index = setNextIndex()
+      this.index = index
     },
 
-    prevSlide() {
-      function setIndex() {
-        let index = this.index - 1
-        if(index < 0){
-          index = this.peopleData.length - 1
-        }
-        return index
+    // prevSlide() {
+    //   function setIndex() {
+    //     let index = this.index - 1
+    //     if(index < 0){
+    //       index = this.peopleData.length - 1
+    //     }
+    //     return index
+    //   }
+    //   this.index = setIndex()
+    // },
+
+    previousSlide () {
+      let index = this.index - 1
+      if (index < 0) {
+        index = this.peopleData.length - 1
       }
-      this.index = setIndex()
+      this.index = index
     },
 
     setSlider () {
       let slider =  setInterval(() => {
-        this.nextSlide()
+        this.afterSlide()
       }, 5000)
       return() => {
         clearInterval(slider)
@@ -61,13 +77,11 @@ export default {
   computed () {},
 
   watch() {
-    "this.index", function () {
+    "index", function () {
       this.setSlider()
     }
     console.log(this.index)
   },
-  created() {
-  }
 }
 </script>
 
