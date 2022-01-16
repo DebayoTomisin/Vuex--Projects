3769529d5d485bdef615873aab5a49a2028492aa
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-16 13:01:30 -0800

    project set up

---
+++ 8-lorem/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
