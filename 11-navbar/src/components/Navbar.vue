942684f5e0b184bb7d197220043710ec90b84b92
Debayo Tomisin <debayotomisin07@gmail.com>
2022-02-05 17:43:57 -0800

    nav bar component

---
+++ 11-navbar/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Nav Bar component</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
