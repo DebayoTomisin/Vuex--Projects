a4d97894dab1f06b9219e30cadb3e919434a2f4b
Debayo Tomisin <debayotomisin07@gmail.com>
2022-02-07 11:46:24 -0800

    naviagation icons

--- 11-navbar/src/components/data.js
+++ 11-navbar/src/components/data.js
@@ -1,3 +1,8 @@
+import twitter from "../assets/twitter-logo-svgrepo-com.svg";
+import facebook from "../assets/facebook-logo-svgrepo-com.svg";
+import behance from "../assets/behance-logo-svgrepo-com.svg";
+import linkedin from "../assets/linkedin-logo-svgrepo-com.svg";
+
 export const links = [
   {
     id: 1,
@@ -29,17 +34,21 @@ export const social = [
   {
     id: 1,
     url: "https://www.twitter.com",
+    icon: twitter,
   },
   {
     id: 2,
-    url: "https://www.twitter.com",
+    url: "https://www.facebook.com",
+    icon: facebook,
   },
   {
     id: 3,
-    url: "https://www.twitter.com",
+    url: "https://www.behance.net/",
+    icon: behance,
   },
   {
     id: 4,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com",
+    icon: linkedin,
   },
 ];
